008aedb94bbc2edf316af472c2f2bf57024261e3
zhaofengjun <zhaofengjun118@yeah.net>
2018-12-19 16:20:22 +0800

    fix

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -8,10 +8,34 @@ Created on Sun Nov  4 18:09:07 2018
 from scipy.io import loadmat
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import random as rd
 import scipy.optimize as opt
-from numpy import random
+ 
+
+def displayData(X):
+    example_width = int(np.round(np.sqrt(np.size(X, 1))))
+    [m,n]=np.shape(X)
+    example_height=int(n/example_width)
+    display_rows=int(np.floor(np.sqrt(m)))
+    display_cols=int(np.ceil(m/display_rows))
+    
+    pad=1    
+    display_array=-np.ones((pad + display_rows * (example_height + pad),pad + display_cols * (example_width + pad)))
+    curr_ex=0
+    for j in range(1,display_rows+1):
+        for i in range(1,display_cols+1):
+            if curr_ex>m:
+                break
+            max_val=np.max(abs(X[curr_ex,:]))
+            colstart=pad + (j - 1)*(example_height + pad)
+            colend=pad + (j - 1)*(example_height + pad)+example_height
+            rowstart=pad + (i - 1)*(example_width + pad)
+            rowend=pad + (i - 1)*(example_width + pad)+example_width
+            display_array[colstart:colend,rowstart:rowend] = X[curr_ex, :].reshape((example_height, example_width)) / max_val
+            curr_ex=curr_ex+1
+        if curr_ex>m:
+            break
+        plt.imshow(display_array.T)
+
 
 def sigmoid(z):
     gz=np.zeros(np.shape(z))
@@ -38,7 +62,84 @@ def predict(Theta1,Theta2,X):
     h1=sigmoid(np.dot(np.hstack((np.ones([m,1]),X)),Theta1.T))
     h2=sigmoid(np.dot(np.hstack((np.ones([m,1]),h1)),Theta2.T))    
     return np.argmax(h2,axis=1)+1
+def gradFunction2(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam):
+#    将y转成one-hot    
+    Theta1=np.reshape(nn_params[0:hidden_layer_size*(input_layer_size+1)],(hidden_layer_size,input_layer_size+1))
+    Theta2=np.reshape(nn_params[hidden_layer_size*(input_layer_size+1):],(num_labels,hidden_layer_size+1))
+    m=np.size(X,0)
+
+    Theta1_grad=np.zeros((np.shape(Theta1)))
+    Theta2_grad=np.zeros((np.shape(Theta2)))     
     
+    X=np.hstack((np.ones([m,1]),X))
+    
+    # 先把theta(1)拿掉，不参与正则化
+    temp1=np.hstack((np.zeros([np.size(Theta1,0),1]),Theta1[:,1:]))
+    temp2=np.hstack((np.zeros([np.size(Theta2,0),1]),Theta2[:,1:]))
+    #计算每个参数的平方，再就求和
+    temp1=np.sum(temp1**2)
+    temp2=np.sum(temp2**2) 
+    
+    delta_1=np.zeros(np.shape(Theta1))
+    delta_2=np.zeros(np.shape(Theta2))
+    
+    for t in range(m):
+        #step1
+        a_1=np.matrix(X[t,:]).T
+#        a_1=np.vstack((np.ones([1,1]),a_1))
+        z_2=np.dot(Theta1,a_1)
+        a_2=sigmoid(z_2)
+        a_2=np.vstack((np.ones([1,1]),a_2))
+        z_3=np.dot(Theta2,a_2)
+        a_3=sigmoid(z_3)
+        #step2
+        err_3=np.zeros([num_labels,1])
+        for k in range(num_labels):
+            err_3[k]=a_3[k]-(y[t]==k+1)+0
+        #step3
+        err_2=np.dot(Theta2.T,err_3)
+        err_2=np.multiply(err_2[1:],sigmoidGradient(z_2))
+        #step4
+        delta_2=delta_2+np.dot(err_3,a_2.T)
+        delta_1=delta_1+np.dot(err_2,a_1.T)
+    #step5
+    Theta1_temp=np.hstack((np.zeros([np.size(Theta1,0),1]),Theta1[:,1:]))
+    Theta2_temp=np.hstack((np.zeros([np.size(Theta2,0),1]),Theta2[:,1:]))
+    Theta1_grad=1/m*delta_1+lam/m*Theta1_temp
+    Theta2_grad=1/m*delta_2+lam/m*Theta2_temp    
+    grad=transformVector(Theta1_grad,Theta2_grad)
+    
+    return grad  
+
+def costFunction2(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam):
+#    将y转成one-hot    
+    Theta1=np.reshape(nn_params[0:hidden_layer_size*(input_layer_size+1)],(hidden_layer_size,input_layer_size+1))
+    Theta2=np.reshape(nn_params[hidden_layer_size*(input_layer_size+1):],(num_labels,hidden_layer_size+1))
+    m=np.size(X,0)
+    J=0
+    y_temp=np.zeros([np.size(y),num_labels])
+    y_temp=onehot(y,num_labels)
+    
+    X=np.hstack((np.ones([m,1]),X))
+    z2=np.dot(X,Theta1.T)
+    a2=sigmoid(z2)
+    a2_temp=np.hstack((np.ones([np.size(a2,0),1]),a2))
+    z3=np.dot(a2_temp,Theta2.T)
+    a3=sigmoid(z3)
+    
+    # 先把theta(1)拿掉，不参与正则化
+    temp1=np.hstack((np.zeros([np.size(Theta1,0),1]),Theta1[:,1:]))
+    temp2=np.hstack((np.zeros([np.size(Theta2,0),1]),Theta2[:,1:]))
+    #计算每个参数的平方，再就求和
+    temp1=np.sum(temp1**2)
+    temp2=np.sum(temp2**2)    
+     
+    cost=y_temp*np.log(a3)+(1-y_temp)*np.log((1-a3))
+    
+    J=-1/m*np.sum(cost)+lam/(2*m)*(temp1+temp2)
+        
+    return J   
+
 
 def nnCostFunction(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam):
 #    将y转成one-hot    
@@ -63,10 +164,8 @@ def nnCostFunction(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,l
     temp2=np.hstack((np.zeros([np.size(Theta2,0),1]),Theta2[:,1:]))
     #计算每个参数的平方，再就求和
     temp1=np.sum(temp1**2)
-    temp2=np.sum(temp2**2)
-    
-#    J=np.sum(np.dot(-y.T,np.log(h))-np.dot((1-y).T,np.log(1-h)))/m+lam*np.dot(theta1.T,theta1)/(2*m)
-        
+    temp2=np.sum(temp2**2)    
+     
     cost=y_temp*np.log(a3)+(1-y_temp)*np.log((1-a3))
     
     J=-1/m*np.sum(cost)+lam/(2*m)*(temp1+temp2)
@@ -102,22 +201,13 @@ def nnCostFunction(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,l
     grad=transformVector(Theta1_grad,Theta2_grad)
     
     return J,grad
-          
-        
-    
-#    for i in range(num_labels):
-#        yi=y_temp[:,i].reshape((1,m))
-#        hi=h[:,i].reshape((m,1))
-#        J=J+np.sum(np.dot(-yi,np.log(hi))-np.dot((1-yi),np.log(1-hi)))
-#        
-#    return J/m+lam*(np.sum(Theta1[:,1:]*Theta1[:,1:])+np.sum(Theta2[:,1:]*Theta2[:,1:]))/(2*m)
-
-
-
-def randInitializeWeights(L_out,L_in):
-#    epsilon_init=np.sqrt(6)/np.sqrt(L_in+L_out)
-#    W=random.random(size=(L_out,L_in+1))*2*epsilon_init-epsilon_init
+def randDebugInitializeWeights(L_out,L_in):
     W=np.sin(range(1,L_out*(L_in+1)+1)).reshape((L_in+1,L_out)).T/10
+    return W
+
+def randInitializeWeights(L_in,L_out):
+    epsilon_init=np.sqrt(6)/np.sqrt(L_in+L_out)
+    W=np.random.random(size=(L_out,L_in+1))*2*epsilon_init-epsilon_init
     return W
 
 def backPropagation(X,y,num_labels,Theta1,Theta2):
@@ -141,21 +231,20 @@ def backPropagation(X,y,num_labels,Theta1,Theta2):
     return D1,D2
 def computeNumericalGradient(J, theta):
     numgrad=np.zeros(np.shape(theta))
-    perturb=np.zeros(np.shape(theta))
-    
+    perturb=np.zeros(np.shape(theta))    
     e=1e-4
     for p in range(np.size(theta)):
         perturb[p]=e
         loss1=J(theta-perturb)
         loss2=J(theta+perturb)
-        numgrad[p]=(loss2[0]-loss1[0])/(2*e)
+        numgrad[p]=(loss2-loss1)/(2*e)
         perturb[p]=0
     return numgrad
         
 def costFunction(input_layer_size,hidden_layer_size,num_labels,X,y,lam):
-    def nnCostFunction2(nn_params):
-        return nnCostFunction(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam)
-    return nnCostFunction2
+    def nnCostFunc(nn_params):
+        return nnCostFunction(nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam)[0]
+    return nnCostFunc  
         
 def transformVector(Theta1,Theta2):
     return np.ravel(np.concatenate((Theta1.reshape(np.size(np.matrix(Theta1)),1),Theta2.reshape(np.size(np.matrix(Theta2)),1)),axis=0))
@@ -168,12 +257,12 @@ def checkNNGradients(lam):
     num_labels=3
     m=5
     
-    Theta1=randInitializeWeights(hidden_layer_size,input_layer_size)
-    Theta2=randInitializeWeights(num_labels,hidden_layer_size)
+    Theta1=randInitializeWeights(input_layer_size,hidden_layer_size)
+    Theta2=randInitializeWeights(hidden_layer_size,num_labels)
 #    Theta1=random.random(size=(hidden_layer_size,input_layer_size+1))*2*0
 #    Theta2=random.random(size=(num_labels,hidden_layer_size+1))*2*0
     
-    X=randInitializeWeights(m,input_layer_size-1)
+    X=randInitializeWeights(input_layer_size-1,m)
     y=np.mod(range(1,m+1),num_labels).T+1
     
     nn_params=transformVector(Theta1,Theta2)    
@@ -195,56 +284,72 @@ if __name__=='__main__':
     hidden_layer_size=25
     num_labels=10
     
-    
+    ##=====Loading and Visualizing Data===========
     filePathData="D:\BaiduNetdiskDownload\mlclass-ex4-jin\ex4data1.mat"
     data=loadData(filePathData)
-    X=data["X"]
+    X=data["X"]    
+    displayData(X[np.random.randint(np.shape(X)[0],size=100),:])
+    
+    ##=====Loading Parameters===========
     y=data["y"]
     m=np.size(X,0)
-    fileTheta="D:\BaiduNetdiskDownload\mlclass-ex3-jin\ex3weights.mat"
+    fileTheta="D:\BaiduNetdiskDownload\mlclass-ex4-jin\ex4weights.mat"
     Theta=loadData(fileTheta)
     
     Theta1=Theta["Theta1"]
     Theta2=Theta["Theta2"]
     
     nn_params=transformVector(Theta1,Theta2) 
+    
+    ##=====Compute Cost===========
     lam=0
+    J,grad = nnCostFunction(nn_params, input_layer_size, hidden_layer_size,num_labels, X, y, lam)
     
-    J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size,num_labels, X, y, lam)
+    print('Cost at parameters (loaded from ex4weights):\n(this value should be about 0.287629)\n', J)
+
+    #=========Implement Regularization
+    lam=1
+    J,grad = nnCostFunction(nn_params, input_layer_size, hidden_layer_size,num_labels, X, y, lam)
     
-#    print("J %f"%J)
-    
+    print('Cost at parameters (loaded from ex4weights): \n(this value should be about 0.383770', J)
+
+    #======Sigmoid Gradient
     g=sigmoidGradient(np.array([1, -0.5,0, 0.5, 1]))
-#    print("sigmoid is %f"%(g))
+    print('Sigmoid gradient evaluated at [1 -0.5 0 0.5 1]:\n  ',g)
     
-#    Theta1=np.zeros([25,401])
-#    Theta2=np.zeros([10,26])
-#    res=nnCostFunction(X,y,10,Theta1,Theta2,1)
-    initial_Theta1=randInitializeWeights(hidden_layer_size,input_layer_size)
-    initial_Theta2=randInitializeWeights(num_labels,hidden_layer_size)
+    #====Initializing Pameters   
     
+    initial_Theta1=randInitializeWeights(input_layer_size,hidden_layer_size)
+    initial_Theta2=randInitializeWeights(hidden_layer_size,num_labels)    
     initial_nn_params=transformVector(initial_Theta1,initial_Theta2) 
+    #=====Implement Backpropagation
     lam=0
     checkNNGradients(lam)
+    #=======Implement Regularization    
+    lam=3
+    checkNNGradients(lam)    
+    debug_J  = nnCostFunction(nn_params, input_layer_size,hidden_layer_size, num_labels, X, y, lam)
+    print("\nCost at (fixed) debugging parameters (w/ lambda = 10): \n(this value should be about 0.576051)\n",debug_J[0])
+   
+    #Training NN   
+    lam=0
+   
+    nn_params=opt.fmin_cg(costFunction2,x0=np.ravel(initial_nn_params),fprime=gradFunction2,args=(input_layer_size,hidden_layer_size,num_labels,X,y,lam),maxiter=50)
     
-#    lam=3
-#    checkNNGradients(lam)
-#    debug_J  = nnCostFunction(nn_params, input_layer_size,hidden_layer_size, num_labels, X, y, lam)
-##    print("debug_J is %f"%debug_J)
-#    
-#    
-#    costFunc=costFunction(input_layer_size,hidden_layer_size,num_labels,X,y,lam)
-#    
-#    [nn_params,cost]=opt.fmin_cg(costFunc, x0=initial_nn_params)
-#    
-#    Theta1=np.reshape(nn_params[0:hidden_layer_size*(input_layer_size+1)-1],(hidden_layer_size,(input_layer_size+1)))
-#    Theta2=np.reshape(nn_params[hidden_layer_size*(input_layer_size+1),(num_labels,hidden_layer_size+1)])
-#    
-#    pred = predict(Theta1, Theta2, X)
+    ##nn_params,cost=TrainLinearReg(initial_nn_params,input_layer_size,hidden_layer_size,num_labels,X,y,lam)
+    
+    Theta1=np.reshape(nn_params[0:hidden_layer_size*(input_layer_size+1)],(hidden_layer_size,(input_layer_size+1)))
+    Theta2=np.reshape(nn_params[hidden_layer_size*(input_layer_size+1):],(num_labels,hidden_layer_size+1))
+    
+    #======Visualize Weights
+    displayData(Theta1[:, 1:])   
+    
+    #======Implement Predict
+    pred = predict(Theta1, Theta2, X)   
     
     
-    
-#    backPropagation(X,y,10,Theta1,Theta2)
+    print("\nTraining Set Accuracy: " ,np.mean((pred == y.ravel())) * 100)
+    #backPropagation(X,y,10,Theta1,Theta2)
     
     
     
